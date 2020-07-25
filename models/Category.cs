7e145c41d6fe3e492658986794ece643d97cbb65
Malik Karatas <malikkaratas7@gmail.com>
2020-07-25 13:24:14 +0300

    v1

---
+++ models/Category.cs
@@ -0,0 +1,12 @@
+namespace blog.models{
+
+    using System;
+    using System.ComponentModel.DataAnnotation;
+
+    public class Category{
+        
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
